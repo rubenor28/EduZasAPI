8c51806d86acbb3d850050b57735b781b9045e61
Omar Roman <omar.roman.2828@gmail.com>
2025-09-22 18:25:18 -0600

    inyeccion de dependencias modularizada

--- src/EduZasAPI.Infraestructure/EduZasAPI.Infraestructure.MinimalAPI/Program.cs
+++ src/EduZasAPI.Infraestructure/EduZasAPI.Infraestructure.MinimalAPI/Program.cs
@@ -1,8 +1,12 @@
+using EduZasAPI.Infraestructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
