ce2c252021e81cfb8f126642d7df3c967e0b25d2
Omar Roman <omar.roman.2828@gmail.com>
2025-09-23 19:44:55 -0600

    Refactor AddUseCase

--- src/EduZasAPI.Application/UseCases/Common/AddUseCase.cs
+++ src/EduZasAPI.Application/UseCases/Common/AddUseCase.cs
@@ -2,48 +2,46 @@ using EduZasAPI.Domain.Common;
 
 namespace EduZasAPI.Application.Common;
 
-/// <summary>
-/// Caso de uso genérico para agregar una nueva entidad.
-/// </summary>
-/// <typeparam name="NE">Tipo de los datos de entrada para la creación.</typeparam>
-/// <typeparam name="E">Tipo de la entidad resultante.</typeparam>
-public class AddUseCase<NE, E> : IUseCaseAsync<NE, E, List<FieldErrorDTO>>
+public class AddUseCase<NE, E>
     where NE : notnull
     where E : notnull
 {
     private readonly ICreatorAsync<E, NE> _creator;
     private readonly IBusinessValidationService<NE> _validator;
 
-    /// <summary>
-    /// Inicializa una nueva instancia de <see cref="AddUseCase{NE, E}"/>.
-    /// </summary>
-    /// <param name="creator">Servicio para crear entidades.</param>
-    /// <param name="validator">Servicio para validar reglas de negocio.</param>
     public AddUseCase(ICreatorAsync<E, NE> creator, IBusinessValidationService<NE> validator)
     {
         _creator = creator;
         _validator = validator;
     }
 
-    /// <summary>
-    /// Ejecuta la operación de agregar de forma asíncrona.
-    /// </summary>
-    /// <param name="request">Datos de entrada para la creación de la entidad.</param>
-    /// <returns>
-    /// Resultado que contiene la entidad creada si fue exitosa,
-    /// o una lista de errores de validación si falló.
-    /// </returns>
-    public async Task<Result<E, List<FieldErrorDTO>>> ExecuteAsync(NE request)
+    public async Task<Result<E, List<FieldErrorDTO>>> ExecuteAsync(
+        NE request,
+        Func<NE, NE>? formatData = null,
+        Func<NE, Result<Unit, List<FieldErrorDTO>>>? extraValidation = null,
+        Func<NE, Task<Result<Unit, List<FieldErrorDTO>>>>? extraValidationAsync = null)
     {
-        var validation = _validator.IsValid(request);
+        var formatted = (formatData ?? (x => x))(request);
 
+        var validation = _validator.IsValid(formatted);
         if (validation.IsErr)
+            return Result<E, List<FieldErrorDTO>>.Err(validation.UnwrapErr());
+
+        if (extraValidation is not null)
         {
-            var errors = validation.UnwrapErr();
-            return Result<E, List<FieldErrorDTO>>.Err(errors);
+            var syncCheck = extraValidation(formatted);
+            if (syncCheck.IsErr)
+                return Result<E, List<FieldErrorDTO>>.Err(syncCheck.UnwrapErr());
         }
 
-        var newRecord = await _creator.AddAsync(request);
+        if (extraValidationAsync is not null)
+        {
+            var asyncCheck = await extraValidationAsync(formatted);
+            if (asyncCheck.IsErr)
+                return Result<E, List<FieldErrorDTO>>.Err(asyncCheck.UnwrapErr());
+        }
+
+        var newRecord = await _creator.AddAsync(formatted);
         return Result<E, List<FieldErrorDTO>>.Ok(newRecord);
     }
 }
