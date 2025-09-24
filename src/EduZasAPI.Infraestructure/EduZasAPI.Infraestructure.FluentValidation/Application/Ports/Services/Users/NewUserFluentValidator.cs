63bd2c997bd67a9319bd3f7a08d21cb3e7ae239e
Omar Roman <omar.roman.2828@gmail.com>
2025-09-23 22:40:53 -0600

    Corregir evaluacion regex en validador

--- src/EduZasAPI.Infraestructure/EduZasAPI.Infraestructure.FluentValidation/Application/Ports/Services/Users/NewUserFluentValidator.cs
+++ src/EduZasAPI.Infraestructure/EduZasAPI.Infraestructure.FluentValidation/Application/Ports/Services/Users/NewUserFluentValidator.cs
@@ -47,15 +47,15 @@ public class NewUserFluentValidator : FluentValidator<NewUserDTO>
 
         RuleFor(x => x.MidName)
             .Must(opt => opt.Match(
-                  name => Regex.IsMatch(UserRegexs.SimpleName, name) ||
-                          Regex.IsMatch(UserRegexs.CompositeName, name),
+                  name => Regex.IsMatch(name, UserRegexs.SimpleName) ||
+                          Regex.IsMatch(name, UserRegexs.CompositeName),
                   () => true
             )).WithMessage("Formato inválido");
 
         RuleFor(x => x.MotherLastname)
             .Must(opt => opt.Match(
-                  name => Regex.IsMatch(UserRegexs.SimpleName, name) ||
-                          Regex.IsMatch(UserRegexs.CompositeName, name),
+                  name => Regex.IsMatch(name, UserRegexs.SimpleName) ||
+                          Regex.IsMatch(name, UserRegexs.CompositeName),
                   () => true
             )).WithMessage("Formato inválido");
     }
