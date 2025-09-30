c28db9226e35ed8ab89ed8eb33cb00a5a621b982
Omar Roman <omar.roman.2828@gmail.com>
2025-09-30 00:27:26 -0600

    agregar opciones a rdm string service

--- src/EduZasAPI.Application/Ports/Services/RandomStringService.cs
+++ src/EduZasAPI.Application/Ports/Services/RandomStringService.cs
@@ -2,21 +2,30 @@ using System.Text;
 
 namespace EduZasAPI.Application.Common;
 
+
+public class RandomStringGeneratorArgs
+{
+    public char[]? AllowedChars { get; init; }
+    public ulong? MaxStrLenght { get; init; }
+}
+
 /// <summary>
 /// Proporciona un servicio para generar cadenas aleatorias.
 /// </summary>
-public class RandomStringGeneratorService : IRandomStringGeneratorService
+public class RandomStringGeneratorService :
+  IRandomStringGeneratorService,
+  IRandomStringGeneratorService<RandomStringGeneratorArgs?>
 {
     /// <summary>
     /// Conjunto de caracteres autorizados para la generación de la cadena.
     /// </summary>
     private readonly char[] _chars;
-    
+
     /// <summary>
     /// Longitud de la cadena aleatoria a generar.
     /// </summary>
-    private readonly uint _tokenLength;
-    
+    private readonly uint _strLength;
+
     /// <summary>
     /// Instancia de la clase Random utilizada para la generación de números aleatorios.
     /// </summary>
@@ -27,24 +36,48 @@ public class RandomStringGeneratorService : IRandomStringGeneratorService
     /// </summary>
     /// <param name="authorizedChars">Una cadena que contiene todos los caracteres permitidos para la generación.</param>
     /// <param name="tokenLength">La longitud deseada para la cadena generada.</param>
-    public RandomStringGeneratorService(string authorizedChars, uint tokenLength)
+    public RandomStringGeneratorService(string authorizedChars, uint strLength)
     {
         this._chars = authorizedChars.ToCharArray();
-        this._tokenLength = tokenLength;
+        this._strLength = strLength;
         this._rdm = new Random();
+    }
+
+    public string Generate()
+    {
+        var builder = new StringBuilder();
+
+        for (uint i = 0; i < _strLength; i++)
+        {
+            var rdmIdx = _rdm.Next(_chars.Length);
+            builder.Append(_chars[rdmIdx]);
+        }
+
+        return builder.ToString();
     }
 
     /// <summary>
     /// Genera una cadena aleatoria de la longitud especificada.
     /// </summary>
     /// <returns>Una cadena de texto aleatoria.</returns>
-    public string Generate()
+    public string Generate(RandomStringGeneratorArgs? opts = null)
     {
         var builder = new StringBuilder();
-        for (uint i = 0; i < _tokenLength; i++)
+
+        var length = opts is not null &&
+                     opts.MaxStrLenght is not null ?
+                     opts.MaxStrLenght :
+                     _strLength;
+
+        var allowedChars = opts is not null &&
+                           opts.AllowedChars is not null ?
+                           opts.AllowedChars.ToCharArray() :
+                           _chars;
+
+        for (uint i = 0; i < length; i++)
         {
-            var rdmIdx = _rdm.Next(_chars.Length);
-            builder.Append(_chars[rdmIdx]);
+            var rdmIdx = _rdm.Next(allowedChars.Length);
+            builder.Append(allowedChars[rdmIdx]);
         }
 
         return builder.ToString();
