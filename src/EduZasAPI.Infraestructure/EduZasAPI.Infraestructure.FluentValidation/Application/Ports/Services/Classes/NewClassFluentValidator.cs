54f41f3dc2777d5ef738392564c5f1294d68df2a
Omar Roman <omar.roman.2828@gmail.com>
2025-09-30 00:29:19 -0600

    Validadores clases

---
+++ src/EduZasAPI.Infraestructure/EduZasAPI.Infraestructure.FluentValidation/Application/Ports/Services/Classes/NewClassFluentValidator.cs
@@ -0,0 +1,27 @@
+using EduZasAPI.Application.Classes;
+using EduZasAPI.Infraestructure.FluentValidation.Application.Common;
+using FluentValidation;
+
+namespace EduZasAPI.Infraestructure.FluentValidation.Application.Classes;
+
+public class NewClassFluentValidator : FluentValidator<NewClassDTO>
+{
+    public NewClassFluentValidator()
+    {
+        RuleFor(c => c.ClassName)
+          .NotEmpty().WithMessage("Campo requerido")
+          .MinimumLength(3).WithMessage("Al menos 3 caracteres");
+
+        RuleFor(c => c.Subject)
+          .Must(s => s.Match(
+              str => str.Trim().Length >= 3,
+              () => true
+          )).WithMessage("Al menos 3 caracteres");
+
+        RuleFor(c => c.Section)
+          .Must(s => s.Match(
+              str => str.Trim().Length >= 3,
+              () => true
+          )).WithMessage("Al menos 3 caracteres");
+    }
+}
