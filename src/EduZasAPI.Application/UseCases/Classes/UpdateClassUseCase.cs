2cc8862bc300daa2542f56fb598cf3e973e09b9b
Omar Roman <omar.roman.2828@gmail.com>
2025-10-04 11:18:50 -0600

    comentar caso de uso

--- src/EduZasAPI.Application/UseCases/Classes/UpdateClassUseCase.cs
+++ src/EduZasAPI.Application/UseCases/Classes/UpdateClassUseCase.cs
@@ -4,10 +4,19 @@ using EduZasAPI.Application.Common;
 
 namespace EduZasAPI.Application.Classes;
 
+/// <summary>
+/// Caso de uso para actualizar una clase.
+/// </summary>
 public class UpdateClassUseCase : UpdateUseCase<ClassUpdateDTO, ClassDomain>
 {
-    private IQuerierAsync<ProfessorClassRelationDTO, ProfessorClassRelationCriteriaDTO> _professorClassQuerier;
+    private readonly IQuerierAsync<ProfessorClassRelationDTO, ProfessorClassRelationCriteriaDTO> _professorClassQuerier;
 
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="UpdateClassUseCase"/>.
+    /// </summary>
+    /// <param name="updater">Servicio para actualizar la entidad de la clase.</param>
+    /// <param name="validator">Servicio para validar los datos de la clase a actualizar.</param>
+    /// <param name="professorClassQuerier">Servicio para consultar la relación entre profesores y clases.</param>
     public UpdateClassUseCase(
         IUpdaterAsync<ClassDomain, ClassUpdateDTO> updater,
         IBusinessValidationService<ClassUpdateDTO> validator,
@@ -17,6 +26,11 @@ public class UpdateClassUseCase : UpdateUseCase<ClassUpdateDTO, ClassDomain>
         _professorClassQuerier = professorClassQuerier;
     }
 
+    /// <summary>
+    /// Realiza validaciones adicionales para la actualización de la clase.
+    /// </summary>
+    /// <param name="value">DTO con los datos de la clase a actualizar.</param>
+    /// <returns>Un resultado que indica si la validación fue exitosa o no.</returns>
     protected async override Task<Result<Unit, UseCaseErrorImpl>> ExtraValidationAsync(ClassUpdateDTO value)
     {
         var result = await _professorClassQuerier.GetByAsync(new ProfessorClassRelationCriteriaDTO
