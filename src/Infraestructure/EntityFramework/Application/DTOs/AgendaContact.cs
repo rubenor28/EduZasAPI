b65079d36de2770e247665952823539e7007288a
Omar Roman <omar.roman.2828@gmail.com>
2025-10-23 17:49:18 -0600

    campo erroneo en bd

--- src/Infraestructure/EntityFramework/Application/DTOs/AgendaContact.cs
+++ src/Infraestructure/EntityFramework/Application/DTOs/AgendaContact.cs
@@ -4,7 +4,7 @@ public partial class AgendaContact
 {
     public ulong AgendaContactId { get; set; }
     public string Alias { get; set; } = null!;
-    public string Notes { get; set; } = null!;
+    public string? Notes { get; set; }
     public ulong AgendaOwnerId { get; set; }
     public ulong ContactId { get; set; }
     public DateTime CreatedAt { get; set; }
